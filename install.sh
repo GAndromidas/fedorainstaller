4c6b1df2f9301f1a5e30f00fd11905c304581dbc
George Andromidas <karlandromidas@gmail.com>
2024-08-13 23:53:15 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,19 @@
 # Description: Script for setting up a Fedora system with various configurations and installations.
 # Author: George Andromidas
 
+# ASCII art
+clear
+echo -e "${CYAN}"
+cat << "EOF"
+ ______         _                  ___           _        _ _           
+|  ____|       | |                |_ _|_ __  ___| |_ __ _| | | ___ _ __ 
+| |__ ___  ____| | ___  _ __ __ _  | || '_ \/ __| __/ _` | | |/ _ \ '__|
+|  __/ _ \/ _` | |/ _ \| '__/ _` | | || | | \__ \ || (_| | | |  __/ |   
+| | |  __/ (_| | | (_) | | | (_| |_| || | | |__) \__\__,_|_|_|\___|_|   
+|_|  \___|\__,_|_|\___/|_|  \__,_(_)_||_| |_|___/
+
+EOF
+
 # Color codes
 RED='\033[0;31m'
 GREEN='\033[0;32m'
